7bbc7f739ce180b6ec25e36f9b2d99e7b0da7764
ravindra rawat <ravinrawat.wd@gmail.com>
2018-11-20 12:55:38 -0700

    added super() to extend properties

--- oop/extends.js
+++ oop/extends.js
@@ -1,13 +1,14 @@
 // superClass
 class Person {
-  constructorna(name, quirkyFact) {
+  constructor(name, email, quirkyFact) {
     this.name = name;
-    this.quirkyfact = this.quirkyfact;
+    this.email = email;
+    this.quirkyFact = quirkyFact;
   }
   // bio
   bio() {
     return `My name is ${this.name} , and here\'s my quirky fact : ${
-      this.quirkyfact
+      this.quirkyFact
     } .`;
   }
 }
@@ -15,6 +16,9 @@ class Person {
 // students
 
 class Student extends Person {
+  constructor(name, email, quirkyFact) {
+    super(name, email, quirkyFact);
+  }
   enrollStudent(cohort) {
     this.cohort = cohort;
   }
@@ -23,6 +27,9 @@ class Student extends Person {
 // mentor
 
 class Mentor extends Person {
+  constructor(name, email, quirkyFact) {
+    super(name, email, quirkyFact);
+  }
   goOnShift() {
     this.onShift = true;
   }
@@ -30,3 +37,23 @@ class Mentor extends Person {
     this.onShift = false;
   }
 }
+
+// student instences
+
+student1 = new Student('rav', 'rav@gmail.com', 'shrimp breeder');
+student2 = new Student('raw', 'raw@gmail.com', 'Juggler');
+student3 = new Student('ravraw', 'ravraw@gmail.com', 'Hunter');
+
+// student mentor instences
+
+mentor1 = new Mentor('Hafiz', 'hafiz@gmail.com', 'hip-hop enthusiast');
+mentor2 = new Mentor('Lee', 'lee@gmail.com', 'loves to bbq');
+mentor3 = new Mentor('Joel', 'joel@gmail.com', 'loves dark chocolate');
+
+console.log(student1.bio());
+console.log(student2.bio());
+console.log(student3.bio());
+
+console.log(mentor1.bio());
+console.log(mentor2.bio());
+console.log(mentor3.bio());
