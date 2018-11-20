831a356044fe8e736d08ceca4b3fd9e41c4dbd50
ravindra rawat <ravinrawat.wd@gmail.com>
2018-11-20 12:33:25 -0700

    aded superclass person

---
+++ oop/extends.js
@@ -0,0 +1,13 @@
+// superClass
+class Person {
+  constructorna(name, quirkyFact) {
+    this.name = name;
+    this.quirkyfact = this.quirkyfact;
+  }
+  // bio
+  bio() {
+    return `My name is ${this.name} , and here\'s my quirky fact : ${
+      this.quirkyfact
+    } .`;
+  }
+}
